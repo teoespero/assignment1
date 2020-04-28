cfd374ba648f0cc7825e1d1b52b5f6471fec45d0
Teo Espero <38012835+teoespero@users.noreply.github.com>
2020-04-28 09:27:04 -0700

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+# ****
+# Program Name: assignment1.py
+# Author: Teo Espero (GIST)
+# Date Written: 20200420
+# Description: This program was created to illustrate one of the output functions
+#              used by Python and some mathematical operations available to it.
+# ****
+
+print("234 * 456 =")
+print(234 * 456)
+
+# - EOF
+
+""" A RECORDING OF THE RUN
+234 * 456 =
+106704
+"""
+
+# comments: it's interesting to note that """ allows you to declare a multiline
+# string in Python (https://towardsdatascience.com/single-double-and-triple-quotes-in-python-7ceea990baf)
